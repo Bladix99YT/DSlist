e0cf2d81fa07e5fd44dbfb0263af8eda7e025bfe
Bladix99YT <maikon15489566@hotmail.com>
2025-01-23 15:37:39 -0400

    Seed Generation

--- src/main/java/com/bladix/dslist/entities/Game.java
+++ src/main/java/com/bladix/dslist/entities/Game.java
@@ -14,29 +14,45 @@ import jakarta.persistence.Table;
 public class Game {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private long id;
+	
+	@Column(name = "title")
 	private String title;
 	
 	@Column(name = "game_year")
 	private Integer year;
+	
+	@Column(name = "genre")
 	private String genre;
-	private String plataform;
+	
+	@Column(name = "platforms")
+	private String platforms;
+	
+	@Column(name = "score")
+	private Double score;
+	
+	@Column(name = "imgUrl")
 	private String imgUrl;
+	
+	@Column(name = "shortDescription")
 	private String shortDescription;
+	
+	@Column(name = "longDescription")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(long id, String title, Integer year, String genre, String plataform, String imgUrl,
+	public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
-		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -74,12 +90,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlataform() {
-		return plataform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlatform(String platforms) {
+		this.platforms = platforms;
+	}
+	
+	public Double getScore() {
+		return score;
+	}
+	
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
